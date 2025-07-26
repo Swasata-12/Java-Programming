e5b41f35b43b046e603a82788086c4248e464b88
Swasata-12 <swasatanandy12@gmail.com>
2025-07-26 23:46:44 +0530

    Enhance ArrayList usage examples and add new operations
    Added examples of creating an ArrayList from another collection, demonstrated removal by value, printing class names, and using for-each loops. Also included sorting of ArrayLists and additional print statements to illustrate various operations.

--- ArrayListinJava.java
+++ ArrayListinJava.java
@@ -5,6 +5,12 @@ public class ArrayListinJava {
         ArrayList<Integer> l1 = new ArrayList<>();
         ArrayList<Integer> l2 = new ArrayList<>(10);
         ArrayList<Integer> l3 = new ArrayList<>(5);
+
+        // Creating an ArrayList from another collection
+        List<String> anotherList = Arrays.asList("Apple", "Banana", "Orange");
+        ArrayList<String> listFromCollection = new ArrayList<>(anotherList);
+        System.out.println(l3.getClass().getName());
+        System.out.println(anotherList.getClass().getName());
         Scanner sc = new Scanner(System.in);
         l2.add(5);
         l2.add(1);
@@ -20,11 +26,14 @@ public class ArrayListinJava {
         l1.add(0, 25); // Inserts an element at the given index
         l1.addAll(0, l2); // merged into another Arraylist
         l1.remove(0); // used to delete or remove an element at a given index
+        l1.remove(Integer.valueOf(58)); // remove the value 58 from the list l1
         l1.set(4, 100); // set the element at the given index
         System.out.println(l1); // printing the arraylist in horizontal manner
         System.out.println(l1.contains(10));
         System.out.println(l1.indexOf(97));
         System.out.println(l1.lastIndexOf(12));
+        System.out.println(l1.size());
+        // anotherList.add(1, "Aman");
 
         // printing the arraylist in vertical manner
         for (int i = 0; i < l1.size(); i++) {
@@ -40,6 +49,15 @@ public class ArrayListinJava {
             System.out.println(l3.get(i));
         }
         System.out.println(l3);
+
+        // Printing the arraylist l3 using for each loop
+        for (int element : l3) {
+            System.out.println(element);
+        }
+        System.out.println(listFromCollection);
+        // Collections.sort(l3);
+        l3.sort(null);
+        System.out.println("After sorting the list is: " + l3);
         sc.close();
     }
 }
